ffb7a0547560056e78b7d50a0441e660f2dd536f
Georgy Stikhanovskiy <gst87@uclive.ac.nz>
2025-11-28 17:51:20 +1300

    Fixed the clicking issue and centered the game canvas

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -7,8 +7,9 @@ SpaceInvaders.StartMenu.prototype = {
 	create: function () {
 		startBG = this.add.image(0, 0, 'titlescreen');
 		startBG.inputEnabled = true;
-		startBG.events.onInputDown.addOnce(this.startGame, this);		
+		startBG.events.onInputDown.addOnce(this.startGame, this);
 		startPrompt = this.add.bitmapText(this.world.centerX, this.world.centerY, 'eightbitwonder', "Protect the lake!", 24);
+		this.input.onTap.addOnce(this.startGame, this);
 		
 	},
 	startGame: function (pointer) {
